e89f81a1a1cdfa703e9735199887ddb00d97a95b
Ivan Diaz <ivan.camilo.d@hotmail.com>
2024-10-01 20:54:09 -0500

    cambio

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
-
 public class Alumno {
+    private String nombre;
+    private int edad;
+    private int curso;
+    private double promedio;
+
+    public Alumno(String nombre, int edad, int curso, double promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCurso() {
+        return this.curso;
+    }
+
+    public void setCurso(int curso) {
+        this.curso = curso;
+    }
+
+    public double getPromedio() {
+        return this.promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public String toString() {
+        return "Alumno{nombre='" + this.nombre + "', edad=" + this.edad + ", curso=" + this.curso + ", promedio=" + this.promedio + "}";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,92 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//Ivan Camilo Diaz Rodriguez ID:935388//
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, Alumno> alumnos = new HashMap();
+        alumnos.put(1, new Alumno("Ivan Camilo Diaz", 17, 11, 3.0));
+        alumnos.put(2, new Alumno("María Antonia Sanchez", 16, 11, 4.2));
+        alumnos.put(3, new Alumno("Alejandra Nieto", 15, 11, 4.5));
+        alumnos.put(4, new Alumno("Juan Rodríguez", 21, 11, 3.3));
+        alumnos.put(5, new Alumno("Luis Garcia", 23, 11, 4.8));
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+        label34:
+        do {
+            System.out.println("\n--- Sistema de Gestión de Alumnos ---");
+            System.out.println("1. Ver datos de el alumno");
+            System.out.println("2. Ver todos los alumnos");
+            System.out.println("3. Agregar un nuevo alumno");
+            System.out.println("4. Remover un alumno");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.print("\nIngrese el numero del alumno: ");
+                    int numero = scanner.nextInt();
+                    if (alumnos.containsKey(numero)) {
+                        System.out.println("Datos del alumno:");
+                        System.out.println(alumnos.get(numero));
+                    } else {
+                        System.out.println("No se encontró un alumno con ese numero.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("\nLista de todos los alumnos:");
+                    Iterator var11 = alumnos.keySet().iterator();
+
+                    while(true) {
+                        if (!var11.hasNext()) {
+                            continue label34;
+                        }
+
+                        Integer key = (Integer)var11.next();
+                        System.out.println("numero: " + key + " - " + String.valueOf(alumnos.get(key)));
+                    }
+                case 3:
+                    System.out.print("\nIngrese el numero del nuevo alumno: ");
+                    int nuevoNumero = scanner.nextInt();
+                    if (alumnos.containsKey(nuevoNumero)) {
+                        System.out.println("Ya existe un alumno con ese numero.");
+                    } else {
+                        scanner.nextLine();
+                        System.out.print("Ingrese el nombre del alumno: ");
+                        String nombre = scanner.nextLine();
+                        System.out.print("Ingrese la edad del alumno: ");
+                        int edad = scanner.nextInt();
+                        System.out.print("Ingrese el grado del alumno: ");
+                        int curso = scanner.nextInt();
+                        System.out.print("Ingrese el promedio del alumno: ");
+                        double promedio = scanner.nextDouble();
+                        alumnos.put(nuevoNumero, new Alumno(nombre, edad, curso, promedio));
+                        System.out.println("Alumno agregado con éxito.");
+                    }
+                    break;
+                case 4:
+                    System.out.print("\nIngrese el Numero del alumno a remover: ");
+                    int alumnoRemover = scanner.nextInt();
+                    if (alumnos.containsKey(alumnoRemover)) {
+                        alumnos.remove(alumnoRemover);
+                        System.out.println("Alumno removido con éxito.");
+                    } else {
+                        System.out.println("No se encontró un alumno con ese ID.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Saliendo del sistema. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while(opcion != 5);
+
+        scanner.close();
     }
 }
